e268d9914caf47b1b361c50a9f6153c16d93f6fd
gabesaleh <gabriel@avarobotics.com>
2017-10-01 20:04:27 -0400

    Add exisiting file

--- lab1.c
+++ lab1.c
@@ -14,8 +14,7 @@
 void updateOnHit();
 void updateOnMiss();
 int whichSet(unsigned int address, unsigned int offsetLength, unsigned int setLength);
-int tagBits(unsigned int address, int numberOfTag);
-int hitWay(unsigned int set, unsigned int way, int *tagArray);
+int tagBits(unsigned int address, unsigned int offsetLength, unsigned int setIndexLength);
 int getNumberOfSets(int cacheSize, int blockSize, int associativity);
 int numberOfBlocks(int cacheSize, int blockSize);
 unsigned int offsetLength(int blockSize);
@@ -31,29 +30,50 @@ int main(int argc, char* argv[])
     int **lruArray;
     FILE *fp;
     int i;
-    unsigned int retVal, K, L, C, set, offset, numberOfSets, numOffsetBits, numIndexBits;
+    unsigned int retVal, K, L, C, set, offset, numberOfSets, numOffsetBits, numIndexBits, numOfTagBits, tag;
+    
+    //int count = 0;
     // open trace file for reading
     // K, L, and C are assigned to the 3 arguements entered
     K = atoi(argv[1]);
     L = atoi(argv[2]);
     C = logTwo(atoi(argv[3])*1000) * 2;
-    //sample trace should be used as 4th arguement
-    fp = fopen(argv[4], "r");
+    fp = fopen(argv[4], "r"); //sample trace should be used as 4th arguement
     
-    
+    //getting correct numbers for sets,offset bits, and index bits
     numberOfSets = getNumberOfSets(C, L, K);
     numOffsetBits= offsetLength(L);
     numIndexBits = setIndexLength(numberOfSets);
     
+    //initializign and allocating memory for tag and lru array
     tagArray = malloc(numberOfSets*sizeof(int*));
     for(int x = 0; x<numberOfSets; x++)
     {
         tagArray[x] = malloc(sizeof(int*)*K);
     }
     
-    printf("Number of sets is ... %d \n", numberOfSets);
-    printf("Number of index bits is ... %d \n", numIndexBits);
+    lruArray = malloc(numberOfSets*sizeof(int*));
+    for(int x = 0; x<numberOfSets; x++)
+    {
+        lruArray[x] = malloc(sizeof(int*)*K);
+    }
+    
+    for (int i = 0; i < numberOfSets; i++) {
+        for(int j = 0; j < K; j++) {
+            lruArray[i][j] = -1;
+        }
+        printf("\n");
+    }
+    
+    /*for (int i = 0; i < numberOfSets; i++) {
+        for(int j = 0; j < K; j++) {
+            printf("%d\t",tagArray[i][j]);
+        }
+        printf("\n");
+    }*/
 
+    
+    
    
    while((retVal = fscanf(fp,"%x",&i))!=EOF)
     {
@@ -72,9 +92,8 @@ int main(int argc, char* argv[])
         set = whichSet(i, numOffsetBits, numIndexBits);
         printf("address = %x\n",i);
         printf("in set %d \n", set);
-        offset = tagBits(i,offsetLength(L));
-        printf("offset bits %d \n", offset);
-        //hitWay(set, offset, tagArray);
+        tag = tagBits(i,numOffsetBits,numIndexBits);
+        printf("tag bits are %d \n", tag);
         
         
         
@@ -101,23 +120,17 @@ int whichSet(unsigned int address, unsigned int offsetLength, unsigned int setLe
     address = address>>offsetLength;
     return address&mask;
 }
+
 //returns tag bits of address
-int tagBits(unsigned int address, int numberOfTag)
+int tagBits(unsigned int address, unsigned int offsetLength, unsigned int setIndexLength)
 {
-    unsigned int mask = 1;
-    unsigned int count = 1;
-    while(count<numberOfTag)
-    {
-        mask = mask + pow(2,count);
-        count++;
-    }
-    return address&mask;
+    return address >> (offsetLength + setIndexLength);
 }
 
 int hitWay(unsigned int set, unsigned int way, int *tagArray)
 {
-    int a = 1;
-    if (a == 1)
+    
+    if ()
     {
         
         return way;
@@ -181,7 +194,7 @@ unsigned int setIndexLength(int numberOfSets)
         x = x + pow(2,length);
         length++;
     }
-    return length-1;
+    return length - 1;
 }
 
 unsigned int logTwo(unsigned int num)
